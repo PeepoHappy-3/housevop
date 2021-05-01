22f3b890017c41256106bd4085ce6d462e5a5bcd
Dmitry <dimasya.qwerty2015@yandex.ru>
2021-05-01 13:27:54 +0300

    шапка, мэин скрин, адаптив, меню бургер

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,1 +1,14 @@
 import './index.css';
+
+
+const burger = document.querySelector('.header__burger');
+const headerMenu = document.querySelector('.header__nav');
+const headerContainer = document.querySelector('.header__container');
+
+burger.addEventListener('click', () => {
+  console.log('click');
+  burger.classList.toggle('active');
+  headerMenu.classList.toggle('active');
+  headerContainer.classList.toggle('active');
+  document.body.classList.toggle('body_locked');
+});
